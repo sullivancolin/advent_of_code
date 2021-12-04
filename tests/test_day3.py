488951380c484c4e203e3de58cb3e1eb172a668e
Colin Sullivan <csullivan@brandwatch.com>
2021-12-04 01:12:55 -0500

    day3 part 1 and 2

---
+++ tests/test_day3.py
@@ -0,0 +1,30 @@
+from advent_of_code.day3.part1 import get_pressure
+from advent_of_code.day3.part2 import get_life_support
+import pytest
+
+
+@pytest.fixture
+def diagnostics() -> list[list[int]]:
+    input = """00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010"""
+
+    diagnostics = [map(int, list(line.strip())) for line in input.split("\n")]
+    return diagnostics
+
+
+def test_pressure(diagnostics) -> None:
+    assert get_pressure(diagnostics) == 198
+
+
+def test_lif_support(diagnostics) -> None:
+    assert get_life_support(diagnostics) == 230
