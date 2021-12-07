2a8b273f6cd73e8c8c346576236d9d21e56230ea
Colin Sullivan <csullivan@brandwatch.com>
2021-12-07 00:50:23 -0500

    day6 part one the hard way

---
+++ tests/test_day6.py
@@ -0,0 +1,8 @@
+from unittest.mock import NonCallableMagicMock
+
+from advent_of_code.day6.part1 import fish_after_interval
+
+
+def test_fish_after_interfal() -> None:
+    start_array = [3, 4, 3, 1, 2]
+    assert fish_after_interval(start_array) == 5934
