ef42b71e296204f38f4fb1c5b4273d3d8be475cc
Colin Sullivan <csullivan@brandwatch.com>
2022-12-06 14:52:48 +0000

    fix test failure

--- tests/test_day6.py
+++ tests/test_day6.py
@@ -10,4 +10,4 @@ def test_signal_start_position() -> None:
 
 def test_message_start_position() -> None:
     result = signal_start_position(input_str, window_size=14)
-    assert result == 7
+    assert result == 19
